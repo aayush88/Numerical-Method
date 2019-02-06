cfabbf5848f4eac490f6723d2e80c851d3b38139
aayush88 <40534586+aayush88@users.noreply.github.com>
2019-02-06 07:55:07 +0545

    Add files via upload

---
+++ simpsons1by3rule.cpp.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<math.h>
+float f(float x){
+	return(1/(1+x*x));
+}
+int main()
+{
+	int i,n;
+	float a,b,h,x[30],y[30],IV;
+	
+	printf("Enter the value of a and b");
+	scanf("%f%f",&a,&b);
+	printf("Enter the value of n:");
+	scanf("%d",&n);
+	
+	h=(b-a)/n;
+	x[0]=a;
+	x[n]=b;
+	for(i=1;i<n;i++)
+	{
+		x[i]=x[i-1]+h;
+		y[i]=f(x[i]);
+	}
+	IV=f(a)+f(b);
+	for(i=1;i<n;i++)
+	{
+		if(i%2==0)
+		IV=IV+2*y[i];
+		else
+		IV=IV+4*y[i];
+	}
+	IV=h*IV/3;
+	printf("The solution of integral value is %f",IV);
+}
+
